88683860a75e744b22dd8684b78ce5ce85d36089
BenHGeier <abgeier@CLOUD9.CABNET>
2017-06-01 10:56:38 +1000

    nserviceBus 'Getting Started' demo solution

---
+++ ClassLibraryThatUsesSharePoint/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryThatUsesSharePoint
+{
+    using Microsoft.SharePoint;
+
+    public class Class1
+    {
+        //This method has been added to 'force' VS to include the SharePoint assemblies in the output folder
+        private void OpenImportXmlFile(Guid siteId, string relativeUrl, string fileUrl)
+        {
+            SPSecurity.RunWithElevatedPrivileges(delegate ()
+            {
+                using (SPSite site = new SPSite(siteId))
+                {
+                    using (SPWeb web = site.OpenWeb(relativeUrl))
+                    {
+                        try
+                        {
+                            var file = web.GetFile(fileUrl);
+                            //OpenImportXmlFile(file.OpenBinaryStream());
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                            throw;
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
